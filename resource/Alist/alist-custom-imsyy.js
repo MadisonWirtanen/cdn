5fa8491c79cb7e9df438b006271b0ae6c964ad8e
MadisonWirtanen <madisonwirtanen@outlook.com>
2023-07-30 00:13:30 +0800

    test

--- resource/Alist/alist-custom-imsyy.js
+++ resource/Alist/alist-custom-imsyy.js
@@ -20,6 +20,39 @@
  * @param {Boolean} isStop - 是否在改变后停止观察
  * @returns {void}
  */
+const observeElementChanges = (selector, callbacks, isStop = true) => {
+  try {
+    const target = document.querySelector("#root");
+    if (!target) {
+      console.error(`未查找到 #root 元素`);
+      return;
+    }
+    const observer = new MutationObserver((mutationsList) => {
+      for (let mutation of mutationsList) {
+        if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+          const matchedElement = target.querySelector(selector);
+          if (matchedElement) {
+            if (Array.isArray(callbacks)) {
+              callbacks.forEach((callback) => {
+                if (typeof callback === "function") {
+                  callback(matchedElement);
+                }
+              });
+            } else if (typeof callbacks === "function") {
+              callbacks(matchedElement);
+            }
+            // 停止观察
+            if (isStop) observer.disconnect();
+          }
+        }
+      }
+    });
+    const config = { childList: true, subtree: true };
+    observer.observe(target, config);
+  } catch (error) {
+    console.error("在观察元素更改时发生错误：", error);
+  }
+};
 
 /**
  * 页脚自定义
@@ -28,7 +61,7 @@
 const customFooter = (el) => {
   try {
     if (el !== null) {
-      el.innerHTML = `<div class="custom-footer"><div class="icon"><a href="https://github.com/MadisonWirtanen"><i class="fa-brands fa-github"></i> </a><a href="https://www.ykwang.top"><i class="fa-solid fa-house"></i> </a><a href="mailto:email@ykwang.top"><i class="fa-solid fa-envelope"></i></a></div><div class="text"><div class="num">Themed by <a href="https://imsyy.top/" target="_blank">無名</a> | Powered by <a href="https://alist.nn.ci/zh/" target="_blank">Alist</a></i></span></div><div class="power">Copyright © 2020 - ${new Date().getFullYear()} <a href="https://ykwang.top/" target="_blank">ykwang</a></div></div></div>`;
+      el.innerHTML = `<div class="text"><div class="num">Themed by <a href="https://imsyy.top/" target="_blank">無名</a> | Powered by <a href="https://alist.nn.ci/zh/" target="_blank">Alist</a></i></span></div><div class="power">Copyright © 2020 - ${new Date().getFullYear()} <a href="https://ykwang.top/" target="_blank">ykwang</a></div></div></div>`;
     }
   } catch (error) {
     console.error("页脚自定义出现问题：" + error);
